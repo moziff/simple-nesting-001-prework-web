9a7e60020051f23ba22a7f5fd136d785677c1785
moziff <matteoziff@gmail.com>
2015-11-20 11:40:06 -0500

    done

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+        return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+        return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+        programmer_hash[:dennis_ritchie][:languages] = "C"
+        return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -79,7 +82,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+        programmer_hash[:yukihiro_matsumoto]={
+          known_for: "Ruby", 
+          languages: ["LISP", "C"]
+        }
+        programmer_hash
     
 end
 
@@ -101,7 +108,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for]= "GUI"
+     programmer_hash
      
 end
 
@@ -121,7 +129,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+        programmer_hash[:dennis_ritchie][:languages]=["C", "Assembly"]
+        programmer_hash
      
 end
 
